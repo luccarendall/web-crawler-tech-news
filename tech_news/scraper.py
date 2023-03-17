bed75eb0e60ec98111177fd3991f4c107c8db065
LuccaRendall <luccarendall2@gmail.com>
2023-03-17 05:42:36 -0300

    Fix: Flake parou de pegar e o arquivo tava fora de formatação. Corrigido agora

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -5,15 +5,16 @@ from parsel import Selector
 
 # Requisito 1
 def fetch(url):
-    headers = { "user-agent": "Fake user-agent" }
+    headers = {"user-agent": "Fake user-agent"}
     try:
         time.sleep(1)
         response = requests.get(url, headers=headers, timeout=3)
         if response.status_code == 200:
-            return response.text # Conteúdo HTML é retornado por .text não .content
+            return response.text
+            # Conteúdo HTML é retornado por .text não .content
         else:
             return None
-    except:
+    except requests.exceptions.Timeout:  # Flake reclamou de deixar só o except
         return None
 
 
@@ -21,14 +22,17 @@ def fetch(url):
 def scrape_updates(html_content):
     selector = Selector(text=html_content)
     lista_vazia = []
-    # busco a tag onde existe o href (a) uso a class dela. Lembra que a leitura é <-
-    # Tipo: eu quero o atributo tal, no caso href(link) que está na classe tal. Se a classe fosse fora da tag precisaria ter aquela cascata lá
+    # busco a tag onde existe o href (a) uso a class dela.
+    # Lembra que a leitura é <-
+    # Tipo: eu quero o atributo tal, no caso href(link) que está na classe tal.
+    # Se a classe fosse fora da tag precisaria ter aquela cascata lá
     links = selector.css('.cs-overlay-link::attr(href)').getall()
 
     if not links:
         return lista_vazia
     else:
         return links
+
 
 # Requisito 3
 def scrape_next_page_link(html_content):
